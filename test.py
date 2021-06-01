e13d2ef01e80dc783ad1b2d5cc0499a5fef6e69d
uglynora <mangos8@yahoo.com>
2021-05-31 20:57:00 -0400

    Added automation test.

---
+++ test.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import time
+import variables
+
+driver =webdriver.Chrome ("/Users/marysotomayor/Desktop/WebBot2/drivers/chromedriver")
+
+
+driver.get("https://www.amazon.com")
+
+
