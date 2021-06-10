e3047241d438cfaae9fbcdf90367fecc6425dd39
uglynora <mangos8@yahoo.com>
2021-06-10 14:52:24 -0400

    Edited test.py.

--- test.py
+++ test.py
@@ -25,23 +25,22 @@ driver.find_element_by_id(variables.submitButton).click()
 driver.find_element_by_id(variables.startButton).click()
 
 ##Compare current date and time to purchase time.
-purchaseTime = datetime.datetime(variables.myDateTime)
+purchaseTime = datetime.datetime(2021,6,10,14,50,00)
 
 while datetime.datetime.now() < purchaseTime :
-    time.sleep(10)
+    time.sleep(1)
     ## Open a new window.
-    driver.execute_script("window.open('');")
-    driver.switch_to.window(driver.window_handles[1])
-    driver.get(variables.shopWebSite)
-    driver.maximize_window()
-    driver.implicitly_wait(10)
-    driver.find_element_by_id(variables.amazonSearchBar).send_keys(variables.mySearch)
-    driver.find_element_by_id(variables.searchButton).click()
-    driver.find_element_by_xpath(variables.desiredProduct).click()
-    driver.find_element_by_id(variables.buyNowButton).click()
-    buyable = driver.find_element_by_id(variables.placeOrderButton)
-    if buyable.is_displayed():
-        driver.switch_to.window(driver.window_handles[0])
-        driver.find_element_by_id(variables.yesButton).click()
-    else:
-        driver.find_element_by_id(variables.noButton).click()
+driver.execute_script("window.open('');")
+driver.switch_to.window(driver.window_handles[1])
+driver.get(variables.shopWebSite)
+driver.maximize_window()
+driver.implicitly_wait(10)
+driver.find_element_by_id(variables.amazonSearchBar).send_keys(variables.mySearch)
+driver.find_element_by_id(variables.searchButton).click()
+driver.find_element_by_class_name(variables.desiredProduct).click()
+buyable = driver.find_element_by_id(variables.buyNowButton)
+if buyable.is_displayed():
+    driver.switch_to.window(driver.window_handles[0])
+    driver.find_element_by_id(variables.yesButton).click()
+else:
+    driver.find_element_by_id(variables.noButton).click()
